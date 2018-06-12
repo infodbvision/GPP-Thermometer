1f6e6a621647caf81e4abecc8a864e701198e2f4
Mees <meesgieling@live.nl>
2018-06-12 16:26:29 +0200

    fixed cors

--- app.js
+++ app.js
@@ -39,6 +39,19 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
 
 /// error handlers
 
